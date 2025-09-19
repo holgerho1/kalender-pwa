30a7900f3e6373178a947e407406a6d4cec090f4
holgerho1 <hosidas5@gmail.com>
2025-09-19 21:36:10 +0200

    Last Sync: 2025-09-19 21:36 (Mobile)

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import { exportierePdf } from "./code/exportPdf.js";
 window.addEventListener("load", () => {
   neuLaden(); // Holt immer aktuelle Daten vom Server
 
-  // PDF-Export-Button verbinden
+  // PDF-Export-Button verbindenn
   const exportBtn = document.getElementById("pdf-export");
   if (exportBtn) {
     exportBtn.onclick = () => {
