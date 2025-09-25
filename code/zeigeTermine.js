643ff32f3ff128ad0e78b8fc66b0476bb5ed3ec4
holgerho1 <hosidas5@gmail.com>
2025-09-25 20:27:03 +0200

    Last Sync: 2025-09-25 20:27 (Mobile)

--- code/zeigeTermine.js
+++ code/zeigeTermine.js
@@ -117,7 +117,7 @@ export function zeigeTermine() {
   input.style.flex = "1";
   input.style.width = "100%";
   input.style.marginTop = "0.5rem";
-  input.style.fontSize = "clamp(1.2rem, 2vw, 1.6rem)"; // ← hier anpassen
+  input.style.fontSize = "calc(1rem + 0.5vw)"; // ← hier anpassen
   input.style.padding = "4px 6px";
   input.style.border = "1px solid #ccc";
   input.style.borderRadius = "4px";
