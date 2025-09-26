47f5c34c11d19d04057f18b989eaa05c3a57db13
holgerho1 <hosidas5@gmail.com>
2025-09-26 18:45:55 +0200

    Last Sync: 2025-09-26 18:45 (Mobile)

--- material/code/db.js
+++ material/code/db.js
@@ -5,3 +5,11 @@ export function ladeProjekte() {
 export function speichereProjekte(projekte) {
   localStorage.setItem("projekte", JSON.stringify(projekte));
 }
+
+export function ladeBereiche() {
+  return JSON.parse(localStorage.getItem("bereiche")) || [];
+}
+
+export function speichereBereiche(bereiche) {
+  localStorage.setItem("bereiche", JSON.stringify(bereiche));
+}
